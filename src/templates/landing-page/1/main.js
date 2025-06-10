af2eb922f56b4c98595492e68ca32dd14dea0921
Oliver Hook <oliver.hook@porsche.digital>
2025-06-10 14:45:53 +0200

    feat: add pause button | oh

--- src/templates/landing-page/1/main.js
+++ src/templates/landing-page/1/main.js
@@ -5,6 +5,8 @@ import './style.css';
 
 const navDrilldown = document.getElementById('nav-drilldown');
 const navButton = document.getElementById('nav-button');
+const pauseButton = document.getElementById('pause-button');
+const video = document.querySelector('video');
 
 navButton.addEventListener('click', () => {
   navDrilldown.open = true;
@@ -16,4 +18,11 @@ navDrilldown.addEventListener('dismiss', (e) => {
 
 navDrilldown.addEventListener('update', (e) => {
   e.target.activeIdentifier = e.detail.activeIdentifier;
+});
+
+pauseButton.addEventListener('click', () => {
+  const isPaused = video.paused;
+  video[isPaused ? 'play' : 'pause']();
+  pauseButton.textContent = isPaused ? 'Pause Video' : 'Play Video';
+  pauseButton.icon = isPaused ? 'pause' : 'play';
 });
